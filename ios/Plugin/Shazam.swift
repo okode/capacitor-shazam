35b6b34861e5262c0b1246085757707527cac41e
Pedro Jorquera <pjorquera@okode.com>
2021-10-31 12:14:57 +0100

    Implemented analysing song

--- ios/Plugin/Shazam.swift
+++ ios/Plugin/Shazam.swift
@@ -3,87 +3,50 @@ import ShazamKit
 
 @objc public class Shazam: NSObject, SHSessionDelegate {
     
+    let session = SHSession()
     let audioEngine = AVAudioEngine()
-    let mixerNode = AVAudioMixerNode()
-    
-    var session: SHSession?
+    var lastMatchID = ""
     
     @objc public func echo(_ value: String) -> String {
         print(value)
         
-        do {
-            try match()
-        } catch {
-            
-        }
+        session.delegate = self
+        startAnalysingAudio()
         
         return value
     }
     
-    func addAudio(buffer: AVAudioPCMBuffer, audioTime: AVAudioTime) {
-        // Add the audio to the current match request.
-        session?.matchStreamingBuffer(buffer, at: audioTime)
-    }
-    
-    func configureAudioEngine() {
-        // Get the native audio format of the engine's input bus.
-        let inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
-        
-        // Set an output format compatible with ShazamKit.
-        let outputFormat = AVAudioFormat(standardFormatWithSampleRate: 48000, channels: 1)
-        
-        // Create a mixer node to convert the input.
-        audioEngine.attach(mixerNode)
-
-        // Attach the mixer to the microphone input and the output of the audio engine.
-        audioEngine.connect(audioEngine.inputNode, to: mixerNode, format: inputFormat)
-        audioEngine.connect(mixerNode, to: audioEngine.outputNode, format: outputFormat)
-            
-        // Install a tap on the mixer node to capture the microphone audio.
-        mixerNode.installTap(onBus: 0,
-                             bufferSize: 8192,
-                             format: outputFormat) { buffer, audioTime in
-            // Add captured audio to the buffer used for making a match.
-            self.addAudio(buffer: buffer, audioTime: audioTime)
-        }
-    }
-    
-    func startListening() throws {
-        // Throw an error if the audio engine is already running.
-        guard !audioEngine.isRunning else { return }
-        let audioSession = AVAudioSession.sharedInstance()
-        
-        // Ask the user for permission to use the mic if required then start the engine.
-        try audioSession.setCategory(.playAndRecord)
-        audioSession.requestRecordPermission { [weak self] success in
-            guard success, let self = self else { return }
-            try? self.audioEngine.start()
-        }
-    }
-    
-    func stopListening() {
-        // Check if the audio engine is already recording.
-        if audioEngine.isRunning {
-            audioEngine.stop()
-        }
-    }
-    
-    func match() throws {
-        // Create a session if one doesn't already exist.
-        if (session == nil) {
-            session = SHSession()
-            session?.delegate = self
+    func startAnalysingAudio() {
+        let inputNode = audioEngine.inputNode
+        let bus = 0
+        inputNode.installTap(onBus: bus, bufferSize: 2048, format: inputNode.inputFormat(forBus: bus)) { (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
+            self.session.matchStreamingBuffer(buffer, at: time)
         }
         
-        // Start listening to the audio to find a match.
-        try startListening()
+        audioEngine.prepare()
+        try! audioEngine.start()
+    }
+    
+    func stopAnalysingAudio() {
+        let inputNode = audioEngine.inputNode
+        let bus = 0
+        inputNode.removeTap(onBus: bus)
+        self.audioEngine.stop()
     }
     
     public func session(_ session: SHSession, didFind match: SHMatch) {
-        
+        if let matchedItem = match.mediaItems.first,
+           let title = matchedItem.title,
+           let artist = matchedItem.artist,
+           let matchId = matchedItem.shazamID, matchId != lastMatchID {
+            lastMatchID = matchId
+            print("I am currently listening to: \(title) by \(artist) - Via ShazamKit")
+        }
     }
     
     public func session(_ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?) {
-        
+        if error != nil {
+            print(error as Any)
+        }
     }
 }
