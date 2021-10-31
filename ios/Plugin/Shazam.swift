46ded35f126d09b65d43b6c796062e7771a84766
Pedro Jorquera <pjorquera@okode.com>
2021-10-31 08:38:20 +0100

    Added initial implementation for matching songs in Shazam default catalog

--- ios/Plugin/Shazam.swift
+++ ios/Plugin/Shazam.swift
@@ -1,8 +1,89 @@
 import Foundation
+import ShazamKit
 
-@objc public class Shazam: NSObject {
+@objc public class Shazam: NSObject, SHSessionDelegate {
+    
+    let audioEngine = AVAudioEngine()
+    let mixerNode = AVAudioMixerNode()
+    
+    var session: SHSession?
+    
     @objc public func echo(_ value: String) -> String {
         print(value)
+        
+        do {
+            try match()
+        } catch {
+            
+        }
+        
         return value
+    }
+    
+    func addAudio(buffer: AVAudioPCMBuffer, audioTime: AVAudioTime) {
+        // Add the audio to the current match request.
+        session?.matchStreamingBuffer(buffer, at: audioTime)
+    }
+    
+    func configureAudioEngine() {
+        // Get the native audio format of the engine's input bus.
+        let inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
+        
+        // Set an output format compatible with ShazamKit.
+        let outputFormat = AVAudioFormat(standardFormatWithSampleRate: 48000, channels: 1)
+        
+        // Create a mixer node to convert the input.
+        audioEngine.attach(mixerNode)
+
+        // Attach the mixer to the microphone input and the output of the audio engine.
+        audioEngine.connect(audioEngine.inputNode, to: mixerNode, format: inputFormat)
+        audioEngine.connect(mixerNode, to: audioEngine.outputNode, format: outputFormat)
+            
+        // Install a tap on the mixer node to capture the microphone audio.
+        mixerNode.installTap(onBus: 0,
+                             bufferSize: 8192,
+                             format: outputFormat) { buffer, audioTime in
+            // Add captured audio to the buffer used for making a match.
+            self.addAudio(buffer: buffer, audioTime: audioTime)
+        }
+    }
+    
+    func startListening() throws {
+        // Throw an error if the audio engine is already running.
+        guard !audioEngine.isRunning else { return }
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        // Ask the user for permission to use the mic if required then start the engine.
+        try audioSession.setCategory(.playAndRecord)
+        audioSession.requestRecordPermission { [weak self] success in
+            guard success, let self = self else { return }
+            try? self.audioEngine.start()
+        }
+    }
+    
+    func stopListening() {
+        // Check if the audio engine is already recording.
+        if audioEngine.isRunning {
+            audioEngine.stop()
+        }
+    }
+    
+    func match() throws {
+        // Create a session if one doesn't already exist.
+        if (session == nil) {
+            session = SHSession()
+            session?.delegate = self
+        }
+        
+        // Start listening to the audio to find a match.
+        try startListening()
+    }
+    
+    public func session(_ session: SHSession, didFind match: SHMatch) {
+        
+    }
+    
+    public func session(_ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?) {
+        
     }
 }
