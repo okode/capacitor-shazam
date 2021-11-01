8198707b2f60bf5d7e1910e038c3b288de3e3eb0
Pedro Jorquera <pjorquera@okode.com>
2021-11-01 12:43:30 +0100

    Refactor

--- ios/Plugin/Shazam.swift
+++ ios/Plugin/Shazam.swift
@@ -1,44 +1,5 @@
 import Foundation
-import ShazamKit
 
-@objc public class Shazam: NSObject, SHSessionDelegate {
-    
-    let session = SHSession()
-    let audioEngine = AVAudioEngine()
-    var lastMatchID = ""
-        
-    func startMatch() {
-        session.delegate = self
-        let inputNode = audioEngine.inputNode
-        let bus = 0
-        inputNode.installTap(onBus: bus, bufferSize: 2048, format: inputNode.inputFormat(forBus: bus)) { (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
-            self.session.matchStreamingBuffer(buffer, at: time)
-        }
-        
-        audioEngine.prepare()
-        try! audioEngine.start()
-    }
-    
-    func stopMatch() {
-        let inputNode = audioEngine.inputNode
-        let bus = 0
-        inputNode.removeTap(onBus: bus)
-        self.audioEngine.stop()
-    }
-    
-    public func session(_ session: SHSession, didFind match: SHMatch) {
-        if let matchedItem = match.mediaItems.first,
-           let title = matchedItem.title,
-           let artist = matchedItem.artist,
-           let matchId = matchedItem.shazamID, matchId != lastMatchID {
-            lastMatchID = matchId
-            print("I am currently listening to: \(title) by \(artist) - Via ShazamKit")
-        }
-    }
-    
-    public func session(_ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?) {
-        if error != nil {
-            print(error as Any)
-        }
-    }
+@objc public class Shazam: NSObject {
+
 }
--- ios/Plugin/ShazamPlugin.swift
+++ ios/Plugin/ShazamPlugin.swift
@@ -1,19 +1,64 @@
 import Foundation
 import Capacitor
+import ShazamKit
 
 @objc(ShazamPlugin)
-public class ShazamPlugin: CAPPlugin {
+public class ShazamPlugin: CAPPlugin, SHSessionDelegate {
 
-    private let implementation = Shazam()
+    let session = SHSession()
+    let audioEngine = AVAudioEngine()
+    var lastMatchID = ""
 
     @objc func startMatch(_ call: CAPPluginCall) {
-        // let value = call.getString("value") ?? ""
-        implementation.startMatch()
+        startMatchImpl()
         call.resolve()
     }
 
     @objc func stopMatch(_ call: CAPPluginCall) {
-        implementation.stopMatch()
+        stopMatchImpl()
         call.resolve()
     }
+    
+    func startMatchImpl() {
+        print("startMatchImpl")
+        session.delegate = self
+        let inputNode = audioEngine.inputNode
+        let bus = 0
+        inputNode.installTap(onBus: bus, bufferSize: 2048, format: inputNode.inputFormat(forBus: bus)) { (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
+            self.session.matchStreamingBuffer(buffer, at: time)
+        }
+        
+        audioEngine.prepare()
+        try! audioEngine.start()
+    }
+    
+    func stopMatchImpl() {
+        print("stopMatchImpl")
+        let inputNode = audioEngine.inputNode
+        let bus = 0
+        inputNode.removeTap(onBus: bus)
+        self.audioEngine.stop()
+        self.lastMatchID = ""
+    }
+    
+    public func session(_ session: SHSession, didFind match: SHMatch) {
+        if let matchedItem = match.mediaItems.first,
+           let title = matchedItem.title,
+           let artist = matchedItem.artist,
+           let matchId = matchedItem.shazamID, matchId != lastMatchID {
+            lastMatchID = matchId
+            print("didFind: \(title) by \(artist)")
+            var data = [String: String]()
+            data["title"] = title
+            data["artist"] = artist
+            print("notifyListeners with event didFind")
+            self.notifyListeners("didFind", data: data)
+        }
+    }
+    
+    public func session(_ session: SHSession, didNotFindMatchFor signature: SHSignature, error: Error?) {
+        print("notifyListeners with event didNotFindMatchFor")
+        self.notifyListeners("didNotFindMatchFor", data: [String: Any]())
+    }
+    
 }
