e12c348ed3d0e0c9b431fa96c2828d81b8b0b255
Pedro Jorquera <pjorquera@okode.com>
2021-11-02 21:31:54 +0100

    Removed unneeded types

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,3 @@
-export interface ShazamMatch {
-  id: string;
-  title: string;
-  artist: string;
-}
-
 export interface ShazamPlugin {
   startMatch(): Promise<void>;
   stopMatch(): Promise<void>;
